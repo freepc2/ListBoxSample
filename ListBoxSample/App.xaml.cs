f0a8997c7d94c86f34321c6aaab3098c1dbac632
freepc2 <freepc2@hotmail.com>
2020-12-08 00:12:55 +0900

    Add project files.

---
+++ ListBoxSample/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ListBoxSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Window window = new Window() { MaxHeight = 600, MaxWidth = 1024 };
+            window.Content = new View.ListBoxView();
+
+            MainWindow = window;
+
+            window.Show();
+        }
+    }
+}
---
+++ ListBoxSample/ViewModel/EmpViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ListBoxSample.ViewModel
+{
+    using System.Collections.ObjectModel;
+    using Model;
+
+    class EmpVlewModel : ObservableCollection<Emp>
+    {
+        public EmpVlewModel()
+        {
+            Add(new Emp() { Empno = 1, Ename = "김길동", Job = "Salesman" });
+            Add(new Emp() { Empno = 2, Ename = "박길동", Job = "Clerk" });
+            Add(new Emp() { Empno = 3, Ename = "정길동", Job = "Clerk" });
+            Add(new Emp() { Empno = 4, Ename = "남길동", Job = "Clerk" });
+            Add(new Emp() { Empno = 5, Ename = "황길동", Job = "Salesman" });
+            Add(new Emp() { Empno = 6, Ename = "홍길동", Job = "Manager" });
+        }
+    }
+}
